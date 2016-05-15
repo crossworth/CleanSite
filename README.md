1389551a495c5f66fb5c7b39556793fd772b56d6
Pedro Henrique <system.pedrohenrique@gmail.com>
2016-05-15 02:15:08 -0300

    Better readme, portable code, database of malicious codes

--- README.md
+++ README.md
@@ -1,21 +1,29 @@
 ###CleanSite
 -----
-Remove trechos de códigos maliciosos de um determinado arquivo/pasta.
 
-##Jusitificativa
-Quem trabalha com Wordpress e principalmente com plugins de terceiros sabe que frequentemente existe problemas de segurança e que em não é incomum a instalação de seu website ficar comprometida, com todos os arquivos apresentando códigos maliciosos.
-Normalmente a solução é reinstalar todo o wordpress e verificar cada plugin para corrigir os problemas de segunraça.
-Porém quando existe um grande número de arquivos fora da instalação padrão do wordpress, como temas ou plugins próprios, talvez o processo de reinstalação não seja possível.
-Com o CleanSite você consegue de forma fácil e rápida verificar todos os arquivos de sua instalação do wordpress ou qualquer outra pasta contendo arquivos sem grandes problemas.
+Search and remove malicious code from websites based on a small database.
+
+## Preview
+
+![Preview](https://raw.githubusercontent.com/systemmovie/CleanSite/master/usage_win.gif)
+
+![Preview](https://raw.githubusercontent.com/systemmovie/CleanSite/master/usage.gif)
 
 
-##Uso
-Para utilizar informe o código que deve ser removido no arquivo `bad_code.txt`
+## About
 
-Para executar: `php run.php /folder/to/my/files`
+People that use wordpress and some websites in general nowadays can face a very serious problem with bad developed plugins, security breach's, shared hosting server with problems and new zero-day exploits.
 
-Você também pode especificar os tipos de arquivos a serem verificados passando um argumento adicional com a extensão
+Server's admin can find some of the websites with malicious code and even the whole server in some cases.
+
+It's quit easy detect this kind of attack and prevent, but the hard work is really get rid off the bad code, some times is just easy reinstall the whole wordpress itself, but ofcourse this is not all the cases.
+
+
+For this reason I have developed this simple `php script` that will load all the previous `.txt` files stored at the `malicious_code` folder and search in a folder for php files that could contain the malicious code, if it find any occurrence it will remove the malicious code.
+
+
+## Use
+Run from the terminal: `php run.php /folder/to/my/files`
+
+**NOTE** You can set the file extension to be verified as the last parameter
 `php run.php /folder/to/my/files php`
-
-
-![Usage](usage.gif)
--- run.php
+++ run.php
@@ -14,60 +14,119 @@
 *
 * CleanSite
 * Pedro Henrique - system.pedrohenrique@gmail.com
+* How to use at https://github.com/systemmovie/CleanSite
 **/
 
+function getTerminalSizeOnWindows() {
+	$output = array();
+	$size = array('width'=>0,'height'=>0);
+	exec('mode',$output);
+	foreach($output as $line) {
+	$matches = array();
+	$w = preg_match('/^\s*columns\:?\s*(\d+)\s*$/i',$line,$matches);
+	if($w) {
+	  	$size['width'] = intval($matches[1]);
+	} else {
+		$h = preg_match('/^\s*lines\:?\s*(\d+)\s*$/i',$line,$matches);
+		if($h) {
+			$size['height'] = intval($matches[1]);
+		}
+	}
+		if($size['width'] AND $size['height']) {
+			break;
+		}
+	}
+	return $size;
+}
 
+
+if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+	$terminal_size = getTerminalSizeOnWindows();
+    $terminal_width = $terminal_size['width'] - 1;
+} else {
+	$terminal_width = exec('tput cols');
+}
+
+
+function print_logo() {
+	print("   ____ _                  ____  _ _         \n");
+	print("  / ___| | ___  __ _ _ __ / ___|(_) |_ ___   \n");
+	print(" | |   | |/ _ \/ _` | '_ \\___ \| | __/ _ \  \n");
+	print(" | |___| |  __/ (_| | | | |___) | | ||  __/  \n");
+	print("  \____|_|\___|\__,_|_| |_|____/|_|\__\___|  \n");
+	print("                                             \n");
+	print("Pedro Henrique - system.pedrohenrique@gmail.com\n");
+
+}
+
+print_logo();
+
+// Recursive glob
 function rglob($pattern, $flags = 0) {
     $files = glob($pattern, $flags); 
-    foreach (glob(dirname($pattern) . '/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir) {
-        $files = array_merge($files, rglob($dir . '/' . basename($pattern), $flags));
+    foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir) {
+    	// avoid auto-removing the database
+    	if (strpos($dir, "malicious_code") === false) {
+			$files = array_merge($files, rglob($dir.'/'.basename($pattern), $flags));
+		}
     }
     return $files;
 }
 
 
-$terminal_width = exec('tput cols');
+$malicious_code_file_names = rglob("./malicious_code/*.txt");
+$malicious_code = [];
 
-$bad_code  = file_get_contents("bad_code.txt");
+print("Loading the malicious code database\n");
+
+foreach($malicious_code_file_names as $file_name) {
+	array_push($malicious_code, file_get_contents($file_name));
+}
+
 $directory = empty($argv[1]) ? "." : $argv[1];
 $file_extension = empty($argv[2]) ? "*" : "*." . $argv[2];
 
-$files = rglob($directory . "/" . $file_extension);
+$dir_name = ($directory == ".") ? "this directory" : $directory;
 
-$files_count = 0;
-foreach ($files as $file) {
-	if (!is_dir($file)) {
-		$files_count++;
-	}
-}
-	
-print("CleanSite - Wordpress\n");
-print("Pedro Henrique - system.pedrohenrique@gmail.com\n");
-print("2015\n");
-print("------------------------------------------------------\n");
+print("Loading files on {$dir_name}\n");
+
+chdir($directory);
+$files_to_scan_file_names = rglob($directory . "/" . $file_extension);
+
+$files_count = count($files_to_scan_file_names);
+
 print("Number of files: " . $files_count . "\n");
-	
+
 $percent = 0;
 $index   = 0;
-foreach ($files as $file) {
-	if (!is_dir($file)) {
-		
 
-		$file_source = file_get_contents($file);
-		$file_source = str_ireplace($bad_code, "", $file_source);
-		file_put_contents($file, $file_source);
+foreach($files_to_scan_file_names as $file_name) {
+
+	if (is_dir($file_name) == false) {
+		$file_data = file_get_contents($file_name);
 
 		$index++;
 		$percent = ($index / $files_count) * 100;
 		$percent = substr($percent, 0, 5);
-	
-		$file_name_short = str_ireplace($directory, "", $file);
+
+		$file_name_short = str_ireplace($directory, "", $file_name);
+		
+		if ((strlen($file_name_short) + 11) > $terminal_width ) {
+			$new_length = (strlen($file_name_short) + 11) - $terminal_width;
+			$file_name_short = substr($file_name_short, $new_length);
+		}
+		
 		$output_str = "[" . $percent . "%] - " . $file_name_short;
 		$output_str = str_pad($output_str, $terminal_width);
 		print($output_str);
 		print("\r");
+
+		foreach($malicious_code as $code) {
+			$file_data = str_ireplace($code, "", $file_data);
+		}
+
+		file_put_contents($file_name, $file_data);
 	}
 }
 
-print("\n");
-print("Done\n");
+print("Done scan\n");
